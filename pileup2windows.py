c3f505c254201e5e2a5b08074a9786535b96bc40
Edward S. Rice <esrice@login.crane.hcc.unl.edu>
2019-03-15 19:15:22 -0500

    Debugging

--- pileup2windows.py
+++ pileup2windows.py
@@ -6,19 +6,20 @@ import argparse
 def parse_args():
     parser = argparse.ArgumentParser(description='Use the output of samtools '
             'mpileup to calculate pi and other statistics in sliding windows.')
-    parser.add_argument('-m', '--min-coverage', type=int, default=10,
+    parser.add_argument('-c', '--min-coverage', type=int, default=10,
             help='minimum read coverage required to consider a position [10]')
-    parser.add_argument('-M', '--max-coverage', type=int, default=100,
+    parser.add_argument('-C', '--max-coverage', type=int, default=100,
             help='maximum read coverage required to consider a position [100]')
     parser.add_argument('-w', '--window-size', type=int, default=20000,
             help='window size, in base pairs [20000]')
     # TODO empirically choose good defaults for these cutoffs
-    parser.add_argument('-h', '--min-het-pct', type=float, default=0.25,
+    parser.add_argument('-m', '--min-het-pct', type=float, default=0.25,
             help='minimum portion of reads supporting alternate allele for '
             'a position to be considered heterozygous [0.25]')
-    parser.add_argument('-H', '--max-het-pct', type=float, default=0.75,
+    parser.add_argument('-M', '--max-het-pct', type=float, default=0.75,
             help='maximum portion of reads supporting alternate allele for '
             'a position to be considered heterozygous [0.75]')
+    return parser.parse_args()
 
 # TODO make sure this function actually does what I want
 # it to, because I copypastad it from a script used for a
@@ -62,7 +63,7 @@ def parse_pileup(instream, min_coverage, max_coverage):
     - number of reads supporting reference
     - number of reads not supporting reference
     """
-    for line in map(lambda b: b.decode("ascii"), instream):
+    for line in instream:
         splits = line.strip().split('\t')
         coverage = int(splits[3])
 
@@ -78,7 +79,9 @@ def parse_pileup(instream, min_coverage, max_coverage):
         yield (chrom, position, number_ref_reads, number_alt_reads)
 
 def main():
-    for pile in parse_pileup(sys.stdout, args.min_coverage, args.max_coverage):
+    args = parse_args()
+
+    for pile in parse_pileup(sys.stdin, args.min_coverage, args.max_coverage):
         chrom, position, number_ref_reads, number_alt_reads = pile
         print('\t'.join(map(str, [chrom, position, number_ref_reads,
             number_alt_reads])))
