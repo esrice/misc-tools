609f5a39031d893956a7f5c5789d455b28a23337
Edward S Rice <edward.rice@missouri.edu>
2019-11-12 14:53:12 -0600

    Finished vcf2csv
    Still will likely need to add some features

--- vcf/vcf2csv.py
+++ vcf/vcf2csv.py
@@ -2,7 +2,6 @@
 
 import argparse
 import re
-import sys
 import vcf
 
 output_field_names = [
@@ -15,15 +14,13 @@ output_field_names = [
     'num_alleles',
     'genes',
     'seq_ontology',
-    'gene_region',
     'effect',
-    'transcript_name',
-    'exon_number',
     'hgvs_c',
     'hgvs_p',
 ]
 
 annotation_re = re.compile("Functional annotations: '(.+)'")
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description=__doc__)
@@ -31,20 +28,23 @@ def parse_args():
                         type=lambda s: vcf.Reader(filename=s))
     return parser.parse_args()
 
+
 def comma_join(fields):
     """
     Converts everything in the list to strings and then joins
     them with commas.
     """
-    return ','.join(map(str,fields))
+    return ','.join(map(str, fields))
 
 
 def main():
+    global output_field_names, annotation_re
+
     args = parse_args()
 
     # make list of ANN sub-field names
     ann_field_names = list(map(str.strip, annotation_re.match(
-        vcf.infos['ANN'].desc).group(1).split('|')))
+        args.vcf.infos['ANN'].desc).group(1).split('|')))
 
     output_field_names += args.vcf.samples
     print(','.join(output_field_names))  # header
@@ -59,8 +59,8 @@ def main():
             record.ALT,
             record.INFO['AF'],
             record.INFO['AC'],
-            record.INFO['AN'],
         ]))
+        output_fields.append(record.INFO['AN'])
         ann_fields = dict(zip(
             ann_field_names,
             record.INFO['ANN'][0].split('|')
@@ -68,13 +68,12 @@ def main():
         output_fields += [ann_fields[n] for n in [
             'Gene_Name',
             'Annotation',
-            'gene_region',  # TODO
-            'effect',  # TODO
-            'transcript_name',  # TODO
-            'exon_number',  # TODO
+            'Annotation_Impact',
             'HGVS.c',
             'HGVS.p',
         ]]
+        output_fields += [record.genotype(s).data.GT for s in args.vcf.samples]
+        print(comma_join(output_fields))
 
 
 if __name__ == '__main__':
