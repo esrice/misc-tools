d52d4022091d698158d8ede17acbbfa6ff582a86
Edward S Rice <edward.rice@missouri.edu>
2021-09-23 17:41:24 -0500

    Debugging

--- busco_scores_from_filtered_table.py
+++ busco_scores_from_filtered_table.py
@@ -21,38 +21,37 @@ def main():
     #   if the BUSCO is "Duplicated", the number of copies present
     buscos = {}
     # read through the file and fill up the dictionary
-    with open(args.busco, "r") as busco_file:
-        for line in busco_file:
-            if not line.starts_with("#"):
-                splits = line.strip().split()
-                busco, status = splits[:2]
-                # if the BUSCO is complete, denote it as such
-                if status == "Complete":
-                    buscos[busco] = "Complete"
-                # if it is marked as "Duplicated", keep track of how many
-                # times we see it
-                elif status == "Duplicated":
-                    if busco not in buscos:
-                        buscos[busco] = 0
-                    buscos[busco] += 1
+    for line in args.busco:
+        if not line.startswith("#"):
+            splits = line.strip().split()
+            busco, status = splits[:2]
+            # if the BUSCO is complete, denote it as such
+            if status == "Complete":
+                buscos[busco] = "Complete"
+            # if it is marked as "Duplicated", keep track of how many
+            # times we see it
+            elif status == "Duplicated":
+                if busco not in buscos:
+                    buscos[busco] = 0
+                buscos[busco] += 1
 
     num_complete_buscos, num_duplicated_buscos = 0, 0
     for value in buscos.values():
         # if a BUSCO is marked as "Duplicated" but only appears once, it's
         # because we removed one or more of the instances, so it's actually
         # a complete BUSCO.
-        if value == "Complete" or len(value) == 1:
+        if value == "Complete" or value == 1:
             num_complete_buscos += 1
         else:
             num_duplicated_buscos += 1
 
     print(
-        "Complete: {} ({}%)".format(
+        "Complete and single-copy: {} ({:.1f}%)".format(
             num_complete_buscos, num_complete_buscos / args.n * 100
         )
     )
     print(
-        "Duplicated: {} ({}%)".format(
+        "Complete and duplicated: {} ({:.1f}%)".format(
             num_duplicated_buscos, num_duplicated_buscos / args.n * 100
         )
     )
